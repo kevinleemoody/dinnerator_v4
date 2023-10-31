f05bdb6066081564d57b3cfae146c692eb7fdbd9
kevinleemoody <Kevin.moody98@gmail.com>
2023-10-30 21:08:44 -0400

    set up tailwind css and added more meals via postman to db.json. Set up mealcard to display dummy data to page.

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -15,11 +15,18 @@ function MealCard() {
     }, []);
 
     return (
-        <div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {meals.map((meal) => (
-                <div key={meal.id}>
-                    <h2>{meal.name}</h2>
-                    {/* Add other meal data here */}
+                <div key={meal.id} className="border border-gray-300 rounded p-4">
+                    <h2 className="text-xl font-semibold mb-2">{meal.name} with {meal.sidedish}</h2>
+                    <h3 className="text-gray-600 mb-2">Ingredients:</h3>
+                    <ul className="list-disc ml-4">
+                        {meal.ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                    <h3 className="text-gray-600 mt-2 mb-2">Recipe:</h3>
+                    <p>{meal.recipe}</p>
                 </div>
             ))}
         </div>
@@ -27,6 +34,41 @@ function MealCard() {
 }
 
 export default MealCard;
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import Axios from 'axios';
+
+// function MealCard() {
+//     const [meals, setMeals] = useState([]);
+
+//     useEffect(() => {
+//         Axios.get('http://localhost:3003/meals')
+//             .then((response) => {
+//                 setMeals(response.data);
+//             })
+//             .catch((error) => {
+//                 console.error('Error fetching data:', error);
+//             });
+//     }, []);
+
+//     return (
+//         <div>
+//             {meals.map((meal) => (
+//                 <div key={meal.id}>
+//                     <h2>{meal.name} with {meal.sidedish}</h2>
+//                     {/* //<h3>{meal.sidedish}</h3> */}
+//                     {/* Add other meal data here */}
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+
+// export default MealCard;
 
 
 
